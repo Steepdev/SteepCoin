dca26345408a5c426e816897565b625b0783c567
Vitali Grabovski <vetal2060@gmail.com>
2018-05-11 04:00:41 +0300

    update20

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -531,7 +531,7 @@ bool CheckCoinStakeTimestamp(int64 nTimeBlock, int64 nTimeTx)
 // Get stake modifier checksum
 unsigned int GetStakeModifierChecksum(const CBlockIndex* pindex)
 {
-    assert (pindex->pprev || pindex->GetBlockHash() == hashGenesisBlock);
+    // assert (pindex->pprev || pindex->GetBlockHash() == hashGenesisBlock);
     // Hash previous checksum with flags, hashProofOfStake and nStakeModifier
     CDataStream ss(SER_GETHASH, 0);
     if (pindex->pprev)
